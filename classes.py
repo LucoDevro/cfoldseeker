bbc363ddc5534312b408565d1d2f176a90371f12
Lucas De Vrieze <99831106+LucoDevro@users.noreply.github.com>
2026-01-28 17:55:39 +0100

    Implemented cblaster session generation

--- classes.py
+++ classes.py
@@ -11,6 +11,21 @@ from pathlib import Path
 from copy import deepcopy
 from concurrent.futures import ThreadPoolExecutor
 from kegg_pull.pull import MultiProcessMultiplePull
+from Bio.PDB.MMCIF2Dict import MMCIF2Dict
+from cblaster.classes import Session
+
+DEFAULTS = {'mode': 'local',
+            'db': ('afdb-proteome', 'afdb-swissprot'),
+            'max_eval': 1,
+            'min_prob': 0,
+            'min_score': 0,
+            'min_seqid': 25,
+            'min_qcov': 70,
+            'gap': 1000,
+            'min_hits': 2,
+            'min_cov_qrs': 2,
+            'require': []
+            }
 
 
 class Hit:
@@ -33,7 +48,7 @@ class Hit:
         self.db: str = db #Structure database the hit was found in
         self.evalue: float = evalue #evalue of the FoldSeek hit
         self.prob: float = prob #FoldSeek hit probability score
-        self.score: float = score #FoldSeek score
+        self.score: int = score #FoldSeek score
         self.seqid: float = seqid #Sequence identity with the query protein
         self.qcov: float = qcov #Query coverage with the query protein
         
@@ -131,6 +146,11 @@ class Cluster:
         common_taxon_id = {h.taxon_id for h in self.hits}
         if len(common_taxon_id) == 0:
             print("Warning! Different taxon IDs found in your cluster.")
+            
+        self.taxon_name: str = self.hits[0].taxon_name
+        common_taxon_name = {h.taxon_name for h in self.hits}
+        if len(common_taxon_name) == 0:
+            print("Warning! Different taxon names found in your cluster.")
         
         return None
     
@@ -141,12 +161,13 @@ class Cluster:
 
 class Search:
     
-    def __init__(self, query, params, mapping_table_path, hits = {}, clusters = []):
+    def __init__(self, query, mapping_table_path, params = DEFAULTS,
+                 hits = [], clusters = []):
         
-        self.query: dict = query # dictionary of query names as keys and file paths as values
-        self.params: dict = params
-        self.hits: list = hits
-        self.clusters: list = clusters
+        self.query: list = query # dictionary of query names as keys and structure filepaths as values
+        self.params: dict = params # dictionary containing the search configuration
+        self.hits: list = hits # list of Hit objects
+        self.clusters: list = clusters # list of Cluster objects
         # The full Uniprot crossref mapping table casted in a polars DataFrame
         self.mapping_table: dict = pl.read_csv("uniprot_kegg_nucl.gz", has_header = False, separator = "\t",
                                                new_columns = ['Uniprot_ID', 'Target_DB', 'ID'])
@@ -154,7 +175,7 @@ class Search:
         return None
     
     def __repr__(self):
-        pass
+        return f"Search of {','.join(list(self.query.keys()))} with {len(self.clusters)} clusters identified"
     
     def run_foldseek(self):
         """
@@ -167,14 +188,12 @@ class Search:
         """
         Submits one structure file to the FoldSeek API and returns the submission ticket in dictionary form.
         """
-        def submit_foldseek_query(query_path):
+        def submit_foldseek_query(query_path, dbs):
             with open(query_path, "rb") as f:
                 files = {"q": f}
-                data = [
-                    ("mode", "3diaa"),
-                    ("database[]", "afdb-proteome"),
-                    ("database[]", "afdb-swissprot")
-                ]
+                data = [("mode", "3diaa")]
+                for db in dbs:
+                    data.append(("database[]", db))
                 response = requests.post(FOLDSEEK_SUBMISSION_URL, files=files, data=data)
                 if response.status_code == 200:
                     return response.json()
@@ -211,9 +230,10 @@ class Search:
             return results
             
         
-        # First submit all query proteins to FoldSeek.
+        # First submit all query proteins to FoldSeek
+        query_foldseek = lambda x: submit_foldseek_query(x, self.params['db'])
         with ThreadPoolExecutor(max_workers = 5) as executor:
-            tickets = dict(zip(self.query.keys(), executor.map(submit_foldseek_query, self.query.values())))
+            tickets = dict(zip(self.query.keys(), executor.map(query_foldseek, self.query.values())))
                 
         # Then wait for the results and retrieve them automatically when completed
         all_job_ids = [ticket['id'] for ticket in tickets.values()]
@@ -225,10 +245,19 @@ class Search:
         return None
     
     
-    def parse_foldseek_results(self, max_eval, min_prob, min_score, min_seqid, min_qcov):
+    def parse_foldseek_results(self):
         """
         Parses the FoldSeek results and creates Hit objects for the passing hits
         """
+        
+        ## Load the thresholds from params
+        max_eval = self.params['max_eval']
+        min_prob = self.params['min_prob']
+        min_score = self.params['min_score']
+        min_seqid = self.params['min_seqid']
+        min_qcov = self.params['min_qcov']
+        
+        ## Parse the FoldSeek results
         all_hits = []
         for query, task in self.hits.items():
             for results_by_db in task['results']:
@@ -241,7 +270,7 @@ class Search:
                     taxon_id = hit_entry['taxId'] # taxon ID
                     evalue = float(hit_entry['eval']) # FoldSeek e-value
                     prob = float(hit_entry['prob']) # FoldSeek probability score
-                    score = float(hit_entry['score']) # FoldSeek hit score
+                    score = int(hit_entry['score']) # FoldSeek hit score
                     seqid = float(hit_entry['seqId']) # Sequence identity with the query protein
                     qcov = (int(hit_entry['qEndPos']) - int(hit_entry['qStartPos']))/int(hit_entry['qLen'])*100 # Sequence covergage
                     
@@ -277,36 +306,39 @@ class Search:
                 
     def crossref(self):
         """
-        Extracts the genomic positional information from a full pulled KEGG record.
+        Crossrefs all hits where possible and retrieves the genomic neighbourhood information (scaffold IDs and coordinates)
         """
         def extract_positional_information(entry: str) -> str:
+            """
+            Extracts the genomic positional information from a full pulled KEGG record.
+            """
             try:
                 return [line for line in entry.split('\n') if 'POSITION' in line][0][12:]
             except IndexError:
                 return None
         
-        """
-        Extracts a mapping dictionary from the full Uniprot crossref mapping table, linking the Uniprot ID (keys) with a list of
-        one or more multiple crossrefs in the provided target database (values).
-        """
         def prepare_mapping_dict(crossref_df: pl.DataFrame, target_db: str) -> dict:
+            """
+            Extracts a mapping dictionary from the full Uniprot crossref mapping table, linking the Uniprot ID (keys) with a list of
+            one or more multiple crossrefs in the provided target database (values).
+            """
             res = crossref_df.filter(crossref_df['Target_DB'] == target_db).drop('Target_DB')
             res = res.group_by('Uniprot_ID').all()
             res = dict(zip(res['Uniprot_ID'], res['ID'].to_list()))
             
             return res
         
-        """
-        Auxiliary function that standardises the hits. In case of multiple values for a given attribute, it splits it into 
-        multiple hits that are identical except for that exact crossref attribute. The split hits are first introduced instead of the old hit before the full
-        hit list is being flattened.
-        In case of an absent or default attribute value, the hit is discarded.
-        
-        Mutates:
-            self.hits: list: the hit list containing Hit objects that represent proteins that are structurally homologous to
-                             one of the query proteins.
-        """
         def sanitise_attr(self, attr):
+            """
+            Auxiliary function that standardises the hits. In case of multiple values for a given attribute, it splits it into 
+            multiple hits that are identical except for that exact crossref attribute. The split hits are first introduced instead of the old hit before the full
+            hit list is being flattened.
+            In case of an absent or default attribute value, the hit is discarded.
+            
+            Mutates:
+                self.hits: list: the hit list containing Hit objects that represent proteins that are structurally homologous to
+                                 one of the query proteins.
+            """
             sanitised_hits = []
             # Loop over all hits, but keep track of the index
             for h in self.hits:
@@ -415,10 +447,16 @@ class Search:
         return None
         
     
-    def identify_clusters(self, intergenic_threshold = 1000, min_hits = 2, min_covered_queries = 2, require = set()):
+    def identify_clusters(self):
         """
         Identifies the gene clusters among the hits.
         """
+        
+        ### Load the requirements from params
+        gap = self.params['gap']
+        min_hits = self.params['min_hits']
+        min_covered_queries = self.params['min_cov_qrs']
+        require = self.params['require']
         
         ### Cluster identification
         ## First, make groups by scaffold and taxon ID
@@ -433,7 +471,7 @@ class Search:
         close_groups = []
         for group, hits in grouped_scaffs.items():
             dists = {pair: Hit.distance(*pair) for pair in it.combinations(hits, 2)}
-            dists = {k:v for k,v in dists.items() if v <= intergenic_threshold}
+            dists = {k:v for k,v in dists.items() if v <= gap}
             close_groups.append(list(dists.keys()))
         
         ## Then identify the clusters by identifying series of connected Hit objects using an undirected network graph
@@ -493,7 +531,7 @@ class Search:
         # Minimum number of covered queries and required queries
         covered_queries = [{h.query for h in cl} for cl in resulting_set]
         resulting_set = [cl for cl,qrs in zip(resulting_set, covered_queries)
-                         if len(qrs) >= min_covered_queries and require <= qrs]
+                         if len(qrs) >= min_covered_queries and set(require) <= qrs]
         
         ### Preparing results
         ## Reorder the clusters by genomic coordinates
@@ -507,19 +545,194 @@ class Search:
         for idx,cl in enumerate(res_objects):
             cl.number = idx+1
         
-        return res_objects
+        self.clusters = res_objects
+        
+        return None
    
     
-    def generate_output(self):
-        pass
+    def generate_cblaster_session(self):
+        """
+        Generates a cblaster session object
+        """
+        def get_sequence_length_from_cif(file):
+            """
+            Determines the sequence length of the structure in the input CIF file.
+            """
+            structure = MMCIF2Dict(file)
+            res_ids = [int(i) for i in structure['_entity_poly_seq.num']]
+            return max(res_ids)
+        
+        def get_clusters_by_id(self, nbs):
+            """
+            Returns the cluster object that has the given cluster number.
+            """
+            return [cl for cl in self.clusters if cl.number in nbs]
+        
+        def get_taxon_name_from_taxon_id(self, txid):
+            """
+            Returns the taxon name associated with a given taxon ID.
+            """
+            return [cl.taxon_name for cl in self.clusters if cl.taxon_id == txid][0]
+        
+        session_dict = {}
+        
+        ### Queries field
+        session_dict['queries'] = list(self.query.keys())
+        
+        ### Query field
+        cblaster_query = {}
+        cblaster_query['indices'] = []
+        ## Query Subjects field
+        cblaster_query_subjects = []
+        previous_end = -500
+        for qry,file in self.query.items():
+            this_subject = {}
+            this_subject['id'] = None
+            this_subject['hits'] = []
+            this_subject['name'] = qry
+            this_subject['ipg'] = None
+            
+            # Sequence length is determined from the number of modelled residues in the CIF file
+            # cblaster session files take a margin of 500 aa between the hits
+            length = get_sequence_length_from_cif(file)
+            this_subject['start'] = previous_end + 500
+            this_subject['end'] = this_subject['start'] + length
+            previous_end = this_subject['end']
+            
+            this_subject['strand'] = 1
+            this_subject['sequence'] = ""
+            
+            cblaster_query_subjects.append(this_subject)
+            
+        cblaster_query['subjects'] = cblaster_query_subjects
+        cblaster_query['intermediate_genes'] = []
+        cblaster_query['score'] = len(self.query.keys())
+        cblaster_query['start'] = 0
+        cblaster_query['end'] = cblaster_query_subjects[-1]['end']
+        cblaster_query['number'] = 0
+        
+        session_dict['query'] = cblaster_query
+        
+        ### Params field
+        cblaster_params = {}
+        cblaster_params['mode'] = self.params['mode']
+        cblaster_params['database'] = list(self.params['db'])
+        cblaster_params['min_identity'] = self.params['min_seqid']
+        cblaster_params['min_coverage'] = self.params['min_qcov']
+        cblaster_params['max_evalue'] = self.params['max_eval']
+        cblaster_params['require'] = self.params['require']
+        cblaster_params['query_file'] = None
+        cblaster_params['rid'] = None
+        cblaster_params['entrez_query'] = ""
+        
+        session_dict['params'] = cblaster_params
+        
+        ### Organisms field
+        cblaster_organisms = {}
+        
+        ## Group the session's cluster records in the same way as they will be structured in the session file,
+        ## which will make processing much easier
+        
+        # Prepare a polars dataframe of taxon IDs (organisms), scaffold IDs and cluster numbers (clusters)
+        taxon_ids = [cl.taxon_id for cl in self.clusters]
+        scaffs = [cl.scaff for cl in self.clusters]
+        cl_nbs = [cl.number for cl in self.clusters]
+        cl_df = pl.DataFrame({'taxon_ids': taxon_ids, 'scaffolds': scaffs, 'cluster_number': cl_nbs})
+        
+        # Group by taxon ID and scaffold ID, and cast into a dictionary
+        grouped_cl_df = cl_df.group_by(['taxon_ids', 'scaffolds']).all()
+        grouped_cl_dict = {(row[0], row[1]): get_clusters_by_id(self, row[2]) for row in grouped_cl_df.iter_rows()}
+        
+        ## Make the cblaster session fields inside out, i.e. populate organisms first with scaffolds (and other attributes),
+        ## then populate the scaffolds with clusters and subjects, then populate the clusters with links to the subjects.
+        for (txid, scaff), clusters in grouped_cl_dict.items():
+            # Create a new organism instance of there's no one for this taxon ID
+            if txid in cblaster_organisms.keys():
+                this_organism = cblaster_organisms[txid]
+            else:
+                this_organism = {'name': get_taxon_name_from_taxon_id(self, txid),
+                                 'strain': f"(txid: {txid})",
+                                 'scaffolds': {}}
+                cblaster_organisms[txid] = this_organism
+            
+            # Create a new scaffold instance of there is no one for this scaffold ID in this taxon
+            if scaff in this_organism['scaffolds'].keys():
+                this_scaffold = this_organism['scaffolds'][scaff]
+            else:
+                this_scaffold = {'accession': scaff,
+                                 'subjects': [],
+                                 'clusters': []}
+                this_organism['scaffolds'][scaff] = this_scaffold
+                
+            # Populate this scaffold with cluster for each cluster identified on this scaffold
+            for cl in clusters:
+                cblaster_this_cluster = {}
+                cblaster_this_cluster['indices'] = list(range(len(cl.hits)))
+                cblaster_this_cluster['subjects'] = []
+                cblaster_this_cluster['intermediate_genes'] = []
+                cblaster_this_cluster['score'] = cl.score
+                cblaster_this_cluster['start'] = cl.start
+                cblaster_this_cluster['end'] = cl.end
+                cblaster_this_cluster['number'] = cl.number
+                this_scaffold['clusters'].append(cblaster_this_cluster)
+                
+                # Populate this scaffold with subjects (hits) for each hit identified on this scaffold
+                # and add the link with the cluster
+                these_hits = cl.hits
+                for hit in these_hits:
+                    cblaster_this_subject = {}
+                    cblaster_this_subject['id'] = None
+                    cblaster_this_subject['hits'] = [{'query': hit.query,
+                                                      'subject': hit.uniprot,
+                                                      'identity': hit.seqid,
+                                                      'coverage': hit.qcov,
+                                                      'evalue': hit.evalue,
+                                                      'bitscore': hit.score}]
+                    cblaster_this_subject['name'] = hit.uniprot
+                    cblaster_this_subject['ipg'] = hit.kegg_id
+                    cblaster_this_subject['start'] = hit.start()
+                    cblaster_this_subject['end'] = hit.end()
+                    cblaster_this_subject['strand'] = int(f"{hit.strand}1")
+                    cblaster_this_subject['sequence'] = None
+                    this_scaffold['subjects'].append(cblaster_this_subject)
+        
+        ## Discard the taxon ID and scaffold ID indexing to get the cblaster session formatting
+        cblaster_organisms = list(cblaster_organisms.values())
+        cblaster_organisms_new = []
+        for organism in cblaster_organisms:
+            organism['scaffolds'] = list(organism['scaffolds'].values())
+            cblaster_organisms_new.append(organism)
+            
+        session_dict['organisms'] = cblaster_organisms_new
+        
+        ### Construct the cblaster Session
+        session = Session.from_dict(session_dict)
+        
+        return session
+        
     
-a = Hit('K0EJ92', 'query1', coords = [[1,10]])
-b = Hit('K0EVU8', 'query2', coords = [[2,20]])
-c = Cluster([a,b], 1)
-s = Search({'query1': '/home/lucas/bin/cfoldseeker_old/AF3_models/fold_sco5088_model_0.cif',
-            'query2': '/home/lucas/bin/cfoldseeker_old/AF3_models/fold_sco5089_model_0.cif'},
-           {}, mapping_table_path = "uniprot_kegg_nucl.gz")
+# Queries
+query = {'query1': '/home/lucas/bin/cfoldseeker_old/AF3_models/fold_sco5088_model_0.cif',
+         'query2': '/home/lucas/bin/cfoldseeker_old/AF3_models/fold_sco5089_model_0.cif'
+         }
+    
+# Search parameters
+params = {'mode': 'local',
+          'db': ('afdb-proteome', 'afdb-swissprot'),
+          'max_eval': 1,
+          'min_prob': 0,
+          'min_score': 0,
+          'min_seqid': 25,
+          'min_qcov': 70,
+          'gap': 1000,
+          'min_hits': 2,
+          'min_cov_qrs': 2,
+          'require': []
+        }
+    
+s = Search(query, "uniprot_kegg_nucl.gz", params = params)
 s.run_foldseek()
-s.parse_foldseek_results(1, 0, 0, 25, 70)
+s.parse_foldseek_results()
 s.crossref()
-clusters = s.identify_clusters(1000)
+s.identify_clusters()
+session = s.generate_cblaster_session()
